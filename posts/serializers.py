865cec1243bac2a7f5c9c15f3d66fa65ffbcf0a3
Thiago Souza <mistersouza@icloud.com>
2024-01-08 11:29:59 +0000

    Add post, followers, following count and set ordering fields to post

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,6 +8,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image_url.url')
+    like_id = serializers.SerializerMethodField()
 
     def validate_image_url(self, value):
         if value.size > 1024 * 2014 * 2:
@@ -26,6 +28,15 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, object):
         request = self.context['request']
         return request.user == object.owner
+    
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Post
@@ -34,6 +45,7 @@ class PostSerializer(serializers.ModelSerializer):
             'owner',
             'is_owner',
             'profile_id',
+            'like_id',
             'profile_image',
             'created_at',
             'updated_at',
